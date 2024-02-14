ad4c30ad73fe1a500838ca97e5490f5c6482b948
Oluwatobi Oluyede <oluwatobioluyede4@gmail.com>
2024-02-14 12:50:28 +0100

    Create generic data hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,11 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGames();
+  const { data: games, error, isLoading } = useGames();
 
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
