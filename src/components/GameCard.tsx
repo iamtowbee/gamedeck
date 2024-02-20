9678475cb179eec830ea121deb9716f4c20aecb6
Oluwatobi Oluyede <oluwatobioluyede4@gmail.com>
2024-02-20 23:04:42 +0100

    Add fallback image for game card loading state

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,6 +5,7 @@ import {
   HStack,
   Heading,
   Image,
+  Img,
   Spacer,
 } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
@@ -19,7 +20,10 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card height="full">
-      <Image src={getCroppedImageUrl(game.background_image)} />
+      <Image
+        src={getCroppedImageUrl(game.background_image)}
+        fallback={<Img />}
+      />
       <CardBody>
         <Flex height="full" direction="column" justifyContent="space-between">
           <Heading fontSize="2xl">{game.name}</Heading>
